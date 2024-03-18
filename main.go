bc837869a9e2cfab8cf674511089e59d3e2c41d6
Fakhri Bashiri <fakhribashiri@Fakhris-MacBook-Pro.local>
2024-03-18 16:37:04 +0700

    add viper configuration

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	"database/sql"
 	"go-bank/api"
 	db "go-bank/db/sqlc"
+	"go-bank/db/util"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple-bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
